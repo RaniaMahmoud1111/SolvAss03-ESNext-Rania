b03a1422ce16c4f6bfab210d9ec6231dfdc93813
Rania Mahmoud <Rania.Mahm321@gmail.com>
2026-02-23 16:21:31 +0200

    Solution of Q2 added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 
-//Q1.
+//Q1. using promise .then .catch
+//our logic 
+//get data from server by fetch 
+//recive it and convet it to easily deal with it 
+//itereate on it and display it 
 function getUsers() {
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(result => result.json())
@@ -17,14 +21,43 @@ function getUsers() {
 
                     </ul>
                     `;
-                    dv.appendChild(userDiv);
+                dv.appendChild(userDiv);
             });
         }).catch(er => console.log(er))
 
 }
-getUsers();//promise - .then
+//getUsers();//promise - .then
 
 
+//Q2  using asynch - await
+async function getData2() {
+    try {
 
+        //get data from server by fetch 
+        //recive it and convet it to easily deal with it 
+        //iterate on it and display it 
+
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+        const users = await response.json();
+
+        //to display it catch the div exist in html then create div and ul for each user when you iterate on them(arr of users )
+        const dv = document.getElementById("users");
+        users.forEach(user => {
+            const userDiv = document.createElement("div");
+                userDiv.innerHTML=`
+                <h2>${user.name}</h2>
+                <ul>
+                <li>username: ${user.name}</li>
+                <li>email: ${user.email}</li>
+                <li>phone: ${user.phone}</li>
+                <li>company-name: ${user.company.name}</li>
+                `;
+                dv.appendChild(userDiv);
+        });
+    } catch (er) { console.log(er); }
+
+}
+
+getData2();//asynch await
 
 
