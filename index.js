791ebc8cd5fc4b9c8ce3f1394c98b0af8fbbae79
Rania Mahmoud <Rania.Mahm321@gmail.com>
2026-02-23 16:05:51 +0200

    Solution of Q1 added

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 
+//Q1.
+function getUsers() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(result => result.json())
+        .then(users => {
+            const dv = document.getElementById("users");
+            users.forEach(user => {
+                let userDiv = document.createElement("div");
+                userDiv.innerHTML = `
+                    <h2>${user.name}</h2>
+                    <ul>
+                    <li>user name: ${user.name}</li>
+                    <li>email : ${user.email}</li>
+                    <li>phone : ${user.phone}</li>
+                    <li>company-name : ${user.company.name}</li>
+
+                    </ul>
+                    `;
+                    dv.appendChild(userDiv);
+            });
+        }).catch(er => console.log(er))
+
+}
+getUsers();//promise - .then
+
+
+
+
+
